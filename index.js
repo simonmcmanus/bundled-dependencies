38d0d3a79b7af55bd9356c35f3e5c44aaeae642d
Simon McManus <mcmanus.simon@gmail.com>
2015-03-23 12:15:47 +0000

    adding command line and confirming dependencies are present before it continues

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ module.exports = function(packageFile) {
   var fs = require('fs');
   var pkg = require(packageFile);
 
-  pkg.bundledDependencies = Object.keys(pkg.dependencies);
+  if(pkg.dependencies) {
+    pkg.bundledDependencies = Object.keys(pkg.dependencies);
 
-  fs.writeFile(packageFile, JSON.stringify(pkg, null, 4), function(error) {
-    if (error) {
-      throw error;
-    }
-    console.log('Package.json updated with the following bundled dependencies:');
-    console.log(pkg.bundledDependencies);
-  });
+    fs.writeFile(packageFile, JSON.stringify(pkg, null, 4), function(error) {
+      if (error) {
+        throw error;
+      }
+      console.log('Package.json updated with the following bundled dependencies:');
+      console.log(pkg.bundledDependencies);
+    });    
+  } else {
+    console.log('No dependencies found');
+  }
 };
